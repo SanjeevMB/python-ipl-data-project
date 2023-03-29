ff400f7ecd890596dac3f4f7372e053555945bd0
Sanjeev Kumar <sanjeev.kumar.23.2@mountblue.tech>
2023-03-29 10:58:03 +0530

    src/server/5-number-of-time-each-team-won-toss-and-match.py
    removing sample data

--- src/server/5-number-of-time-each-team-won-toss-and-match.py
+++ src/server/5-number-of-time-each-team-won-toss-and-match.py
@@ -22,26 +22,3 @@ with open('../data/matches.csv', 'r') as matchCsvFile:
 with open('../public/output/5-number-of-time-each-team-won-toss-and-won-match.json', 'w') as matchAndTossWonCountjson:
 
     matchAndTossWonCountjson.write(dumps(eachTeamWonTossAndMatches))
-
-
-
-iplMatch = {
-    "id": "1", 
-    "season": "2017", 
-    "city": "Hyderabad", 
-    "date": "2017-04-05", 
-    "team1": "Sunrisers Hyderabad", 
-    "team2": "Royal Challengers Bangalore", 
-    "toss_winner": "Royal Challengers Bangalore", 
-    "toss_decision": "field", 
-    "result": "normal", 
-    "dl_applied": "0", 
-    "winner": "Sunrisers Hyderabad", 
-    "win_by_runs": "35", 
-    "win_by_wickets": "0", 
-    "player_of_match": "Yuvraj Singh", 
-    "venue": "Rajiv Gandhi International Stadium, Uppal", 
-    "umpire1": "AY Dandekar", 
-    "umpire2": "NJ Llong", 
-    "umpire3": ""
-    }
